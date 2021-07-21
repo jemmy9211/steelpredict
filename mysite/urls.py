b7a0f663a8127bee4da2b599a7f4e28e96a113a8
jemmy9211 <jemmy9211@gmail.com>
2021-07-21 17:12:23 +0800

    urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , include('steels.urls')),
+    path('upload/' , include('steels.urls')),
     
 ]
